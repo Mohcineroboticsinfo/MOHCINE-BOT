114943af70ab145da3bb9489add34052015660eb
Mohcineroboticsinfo <147176774+Mohcineroboticsinfo@users.noreply.github.com>
2024-02-15 21:38:49 +0100

    Create dlqrn.js

---
+++ plugins/dlqrn.js
@@ -0,0 +1,42 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  conn.reply(m.chat, ` QUR'AN BOT: By ${author} || ${wm}
+
+
+
+الحزب 1  ☚ http://j.mp/2b8SiNO
+الحزب 2  ☚ http://j.mp/2b8RJmQ
+الحزب 3  ☚ http://j.mp/2bFSrtF
+الحزب 4  ☚ http://j.mp/2b8SXi3
+الحزب 5  ☚ http://j.mp/2b8RZm3
+الحزب 6  ☚ http://j.mp/28MBohs
+الحزب 7  ☚ http://j.mp/2bFRIZC
+الحزب 8  ☚ http://j.mp/2bufF7o
+الحزب 9  ☚ http://j.mp/2byr1bu
+الحزب 10 ☚ http://j.mp/2bHfyUH
+الحزب 11 ☚ http://j.mp/2bHf80y
+الحزب 12 ☚ http://j.mp/2bWnTby
+الحزب 13 ☚ http://j.mp/2bFTiKQ
+الحزب 14 ☚ http://j.mp/2b8SUTA
+الحزب 15 ☚ http://j.mp/2bFRQIM
+الحزب 16 ☚ http://j.mp/2b8SegG
+الحزب 17 ☚ http://j.mp/2brHsFz
+الحزب 18 ☚ http://j.mp/2b8SCfc
+الحزب 19 ☚ http://j.mp/2bFSq95
+الحزب 20 ☚ http://j.mp/2brI1zc
+الحزب 21 ☚ http://j.mp/2b8VcBO
+الحزب 22 ☚ http://j.mp/2bFRxNP
+الحزب 23 ☚ http://j.mp/2brItxm
+الحزب 24 ☚ http://j.mp/2brHKw5
+الحزب 25 ☚ http://j.mp/2brImlf
+الحزب 26 ☚ http://j.mp/2bFRHF2
+الحزب 27 ☚ http://j.mp/2bFRXno
+الحزب 28 ☚ http://j.mp/2brI3ai
+الحزب 29 ☚ http://j.mp/2bFRyBF
+الحزب 30 ☚ http://j.mp/2bFREcc
+`.trim(), m) 
+}
+handler.help = ['hizb']
+handler.tags = ['islamic']
+handler.command = /^(hizb)$/i
+
+export default handler
